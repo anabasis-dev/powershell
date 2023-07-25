1316e54725c89762fd6cabad192d0e6cfcd27d42
Anabasis <136603493+anabasis-global@users.noreply.github.com>
2023-07-25 09:34:39 +0300

    Create network.ps1

---
+++ network.ps1
@@ -0,0 +1,15 @@
+  # NetAdapter
+    #"Configuring dns" | Out-Serial
+$InterfaceIndex = Get-NetAdapter | Select-Object -ExpandProperty InterfaceIndex
+
+$MyIP = Get-NetIPAddress -InterfaceIndex $InterfaceIndex -AddressFamily IPv4 | `
+        Select-Object -ExpandProperty IPAddress
+    # Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses "$MyIP,127.0.0.1"
+
+    $DHCPServer = Get-WmiObject Win32_NetworkAdapterConfiguration | `
+        Where-Object InterfaceIndex -eq $InterfaceIndex | `
+        Select-Object -ExpandProperty "DHCPServer"
+    Set-DnsServerForwarder $DHCPServer
+
+    "Restarting" | Out-Serial
+    Restart-Computer -Force
