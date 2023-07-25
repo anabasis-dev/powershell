fa4785bdd25df6e49e149c9310fb95eb05516053
Anabasis <136603493+anabasis-global@users.noreply.github.com>
2023-07-25 09:22:04 +0300

    Update user.ps1

--- user.ps1
+++ user.ps1
@@ -1,11 +1,17 @@
 
-New-ADOrganizationalUnit -Name "OrganizationalUnit1" -Path "DC=$domainComponent0,DC=$domainComponent1"
-New-ADOrganizationalUnit -Name "OrganizationalUnit2" -Path "DC=$domainComponent0,DC=$domainComponent1"
+$domainComponent0=tab
+$domainComponent1=local
 
+function createOU($name) {
+  New-ADOrganizationalUnit -Name $name -Path "DC=$domainComponent0,DC=$domainComponent1"
+#New-ADOrganizationalUnit -Name "OrganizationalUnit2" -Path "DC=$domainComponent0,DC=$domainComponent1"
+}
 
 
 
 $password = (ConvertTo-SecureString P@$$w0rd -AsPlainText -Force)
-
-New-ADUser -DisplayName:"user1" -GivenName:"user1" -Name:"user1" -Path:"OU=OrganizationalUnit1,DC=$domainComponent0,DC=$domainComponent1" -SamAccountName:"user1" -Surname:"user1" -Type:"user" -AccountPassword  -Enabled $true
-New-ADUser -DisplayName:"user2" -GivenName:"user2" -Name:"user2" -Path:"OU=OrganizationalUnit2,DC=$domainComponent0,DC=$domainComponent1" -SamAccountName:"user2" -Surname:"user2" -Type:"user" -AccountPassword (ConvertTo-SecureString P@$$w0rd -AsPlainText -Force) -Enabled $true
+function generateUsername($fullname) { 
+  # $fullname
+  # New-ADUser -DisplayName:"user1" -GivenName:"user1" -Name:"user1" -Path:"OU=Users,DC=$domainComponent0,DC=$domainComponent1" -SamAccountName:"user1" -Surname:"user1" -Type:"user" -AccountPassword  -Enabled $true
+}
+#New-ADUser -DisplayName:"user2" -GivenName:"user2" -Name:"user2" -Path:"OU=OrganizationalUnit2,DC=$domainComponent0,DC=$domainComponent1" -SamAccountName:"user2" -Surname:"user2" -Type:"user" -AccountPassword (ConvertTo-SecureString P@$$w0rd -AsPlainText -Force) -Enabled $true
