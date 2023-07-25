3e6038dcc9c5e911a7853e47615fda00a5069456
Anabasis <136603493+anabasis-global@users.noreply.github.com>
2023-07-25 17:28:21 +0300

    Create Get-UserNameFromFullName.ps1

---
+++ Get-UserNameFromFullName.ps1
@@ -0,0 +1,44 @@
+
+
+let ru = @{ А=A, Б=B, В=V, Г=G, Д=D, Е=E, Ё=E, Ж=ZH, З=Z, И=I, Й=I }
+
+
+function Get-UserNameFromFullName
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Position = 0, Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]
+        $FullName,
+
+        [Parameter(Position = 1, ValueFromPipelineByPropertyName)]
+        [switch]
+        $ReverseNamePositions = $false
+    )
+    
+    process
+    {    
+        if ($ReverseNamePositions)
+        {
+            $Front = -1
+            $End = 0
+        }
+        else
+        {
+            $Front = 0
+            $End = -1
+        }
+
+        $UserNameComponents = $FullName.Trim().ToLower().Split(' ')
+
+        $UserName = '{0}.{1}' -f $UserNameComponents[$Front], $UserNameComponents[$End]
+
+        $UserName -replace 'č', 'c' `
+                  -replace 'č', 'c' `
+                  -replace 'ć', 'c' `
+                  -replace 'đ', 'dj' `
+                  -replace 'š', 's' `
+                  -replace 'ž', 'z'
+    }
+}
